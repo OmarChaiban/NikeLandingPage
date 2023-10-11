03616261f1a29a08c18aa1cd1bbb3940683e81d8
OmarChaiban <omarchaiban21@gmail.com>
2023-10-11 11:53:51 +0300

    Initial commit

---
+++ src/Components/Button.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Button({label, iconUrl,backgroundColor,borderColor,textColor,fullWidth}) {
+  return (
+    <button
+    className={`flex justify-center items-center
+     gap-2 px-7 py-4 font-montserrat border
+     text-lg leading-none
+
+     ${ backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}`
+     :"bg-coral-red text-white border-coral-red"
+      }
+     rounded-full ${fullWidth && 'w-full'}"}`}
+     >
+       {label}
+       {iconUrl && 
+       <img src={iconUrl} alt="arrow right"
+       className='ml-2 rounded-full w-5 h-5'/>
+       }
+    </button>
+  )
+}
+
+export default Button
---
+++ src/Sections/index.js
@@ -0,0 +1,10 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import Services from "./Services";
+
+export{CustomerReviews,Footer, Hero, PopularProducts,SuperQuality,Subscribe,SpecialOffer,Services}
