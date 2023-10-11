663b4d0d9f00d0372678a78dce3368d90efceab5
OmarChaiban <omarchaiban21@gmail.com>
2023-10-11 14:07:21 +0300

    add: deploy workflow

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({label, iconUrl,backgroundColor,borderColor,textColor,fullWidth}) {
   return (
-    <Button
+    <button
     className={`flex justify-center items-center
      gap-2 px-7 py-4 font-montserrat border
      text-lg leading-none
@@ -17,7 +17,7 @@ function Button({label, iconUrl,backgroundColor,borderColor,textColor,fullWidth}
        <img src={iconUrl} alt="arrow right"
        className='ml-2 rounded-full w-5 h-5'/>
        }
-    </Button>
+    </button>
   )
 }
 
